8110a526c8fbdaa834f055f0d76de5a818c00c04
Harshvardhan <121004255+Harshvardhanabc@users.noreply.github.com>
2023-01-29 13:22:41 +0530

    Add files via upload

---
+++ VotingSystem.py
@@ -0,0 +1,18 @@
+for i in range(100000000000000000):
+    print("These are some opposite Parties in election")
+    print(f"1. BJP\n2. BSP\n3. SP")
+    print("Enter your party name = ")
+    x=str(input(""))
+    if x=="BJP":
+        print("Your vote Successfully submitted in BJP")
+        print("Thank you")
+    elif x=="BSP":
+        print("Your vote Successfully submitted in BSP")
+        print("Thank you")
+    elif x=="SP":
+        print("Your vote Successfully submitted in SP")
+        print("Thank you")
+    else:
+        print("Incorrect Name Please try again")
+
+print("Thank you")
